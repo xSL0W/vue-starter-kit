29523bbc1102c4e4205db993aefbbc46bfd8e159
Tony Lea <tony@devdojo.com>
2025-02-14 12:17:14 -0500

    Adding final updates to make this version TW4 compatible

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import vue from '@vitejs/plugin-vue';
-import autoprefixer from 'autoprefixer';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
-import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
@@ -19,15 +18,11 @@ export default defineConfig({
                 },
             },
         }),
+        tailwindcss(),
     ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './resources/js'),
-        },
-    },
-    css: {
-        postcss: {
-            plugins: [tailwindcss, autoprefixer],
         },
     },
 });
